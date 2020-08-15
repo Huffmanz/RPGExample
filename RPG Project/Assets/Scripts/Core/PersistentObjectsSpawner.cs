e08044f49b725c17b71c609856e749eb51f88151
Zach Huffman <huffmanz2012@gmail.com>
2020-08-15 09:01:26 -0500

    Fader

--- RPG Project/Assets/Scripts/Core/PersistentObjectsSpawner.cs
+++ RPG Project/Assets/Scripts/Core/PersistentObjectsSpawner.cs
@@ -5,13 +5,13 @@ namespace RPG.Core
 {
     public class PersistentObjectsSpawner : MonoBehaviour{
         [SerializeField] GameObject persistentObjectPrefab;
-        static bool hasSpawned=false;
+        static bool hasSpawned = false;
         private void Awake(){
             if(hasSpawned){
                 return; 
             }
             SpawnPersistentObjects();
-            hasSpawned=true;
+            hasSpawned = true;
         }
 
         private void SpawnPersistentObjects()
